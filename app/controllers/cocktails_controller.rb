3a382421d48295d98791b5c2db1ce4af023791cc
Stanislas de Skowronski <stanislas.deskowronski@gmail.com>
2020-05-14 16:11:18 +0200

    Hero Cocktails

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,30 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = @cocktail.doses
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new # needed to instantiate the form_for
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,17 @@ require 'json'
 require 'open-uri'
 require 'faker'
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-drinks_serialized = open(url).read
-drinks = JSON.parse(drinks_serialized)
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# drinks_serialized = open(url).read
+# drinks = JSON.parse(drinks_serialized)
 
-puts 'Creating ingredients from the JSON.'
-drinks["drinks"].each do |element|
-  ingredient = Ingredient.new(name: element["strIngredient1"])
-  ingredient.save!
-  puts "Ingredient added."
-end
+# puts 'Creating ingredients from the JSON.'
+
+# drinks["drinks"].each do |element|
+#   ingredient = Ingredient.new(name: element["strIngredient1"])
+#   ingredient.save!
+#   puts "Ingredient added."
+# end
 
 puts 'All ingredients added!'
 
